eecead591cbcd9bbf841c623d5dd13ed531d3ae6
LordSpigot <lordspigot@gmail.com>
2026-09-20 10:10:03 +0200

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,6 @@
+@rem Gradle wrapper launcher
+@echo off
+set DIRNAME=%~dp0
+set APP_HOME=%DIRNAME%
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
